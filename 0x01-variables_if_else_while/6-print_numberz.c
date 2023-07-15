8ad7125f65acd5f038f51314435de81b85421e46
PalesaKarabo <palesakarabo21@gmail.com>
2023-07-15 05:59:58 -0700

    Task6.

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Task 6
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int d;
+
+	for (d = '0'; d <= '9'; d++)
+	putchar(d);
+	putchat("\n")
+
+	return (0);
+}
