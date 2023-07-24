1caef699d0150a79e4d8299a45deb36d6411fe11
PalesaKarabo <palesakarabo21@gmail.com>
2023-07-24 15:23:34 -0700

    task6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -7,22 +7,14 @@
  */
 void puts2(char *str)
 {
-	int longi = 0;
-	int t = 0;
-	char *y = str;
-	int o;
+	int i;
 
-	while (*y != '\0')
+	for (i = 0; str[i] != '\0'; i++)
 	{
-		y++;
-		longi++;
+		if (i % 2 == 0)
+		{
+			_putchar(str[i]);
+		}
 	}
-	t = longi - l;
-	for (o = 0 ; o <= t ; o++)
-	{
-		if (o % 2 == 0)
-	{
-		_putchar(str[0]);
-	}
-	}
+	_putchar('\n');
 }
