d0cb985f09afdf9b3dde89c0021037a6e0f8f1d6
PalesaKarabo <palesakarabo21@gmail.com>
2023-08-16 04:45:10 -0700

    task2

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,7 +1,7 @@
-#include "function_pointer.h"
+#include "function_pointers.h"
 /**
  * int_index - return ondex if comparison = true, else -1
- * array: array
+ * @array: array
  * @size: size of elemnts in array
  * @cmp: pointer to func of one of the 3 in main
  * Return: 0
