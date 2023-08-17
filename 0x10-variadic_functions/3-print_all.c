06cac4956a82853ba19c9dda3ffcf189d3ecc59c
PalesaKarabo <palesakarabo21@gmail.com>
2023-08-17 02:34:41 -0700

    update

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -22,13 +22,13 @@ void print_all(const char * const format, ...)
 			switch (format[i])
 			{
 				case 'c':
-					print("%s%c", sep, va_arg(list, int));
+					printf("%s%c", sep, va_arg(list, int));
 					break;
 				case 'i':
 					printf("%s%d", sep, va_arg(list, int));
 					break;
 				case 'f':
-					print("%s%f", sep, va_arg(list, double));
+					printf("%s%f", sep, va_arg(list, double));
 					break;
 				case 's':
 					str = va_arg(list, char *);
