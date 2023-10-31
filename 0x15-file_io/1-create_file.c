9482ee07bf0e08e4852cbbaf148e4a9d18d4e8fb
PalesaKarabo <PalesaKarabo21@gmail.com>
2023-10-31 10:08:20 -0700

    task

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -3,10 +3,10 @@
 /**
  * create_file - Creates a file.
  * @filename: A pointer to the name of the file to create.
- * @text_context: A pointer to a bstring to write to the file.
+ * @text_content: A pointer to a string to write to the file.
  *
  * Return: If the function fails - -1.
- * Otherwise -1.
+ *         Otherwise - 1.
  */
 int create_file(const char *filename, char *text_content)
 {
